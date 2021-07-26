b7181b819c597cc438c9c0595f22282d541beb72
llaum <llaum@DESKTOP-NOTS01L>
2021-07-26 23:31:34 +0900

    20210726

--- finalProject20210723/py01_samsungMarketPassingTEST.py
+++ finalProject20210723/py01_samsungMarketPassingTEST.py
@@ -3,15 +3,15 @@ from http.client import HTTPSConnection
 from bs4 import BeautifulSoup
 
 # https://finance.naver.com/item/sise_day.nhn?code=005930&page=2
-
+# https://finance.naver.com/item/sise.nhn?code=005930
 
 con = HTTPSConnection("finance.naver.com")
-con.request("GET", "/item/sise_day.nhn?code=005930&page=2" )
+con.request("GET", "/item/sise.nhn?code=005930")
 rb = con.getresponse().read()
 print(rb)
 ssMarketData = BeautifulSoup(rb, "html.parser", from_encoding = "euc-kr")
 print(ssMarketData)
-# ssMarketDatas = ssMarketData.select("iframe html body table.type2 tbody tr td span")
+# ssMarketDatas = ssMarketData.select("#document")
 # print(ssMarketDatas)
 # for m in ssMarketDatas:
     # print(m.text.strip())
